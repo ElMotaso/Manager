3670687fe09874211dacb67226583740183d288e
Thomas Moissl <thomas.m.moissl@gmail.com>
2025-05-23 12:08:19 +0200

    Auslagern der kommentierten Runner Bruchstücke in RunnerDeprecated.cs

--- src/Scripts/Runner.cs
+++ src/Scripts/Runner.cs
@@ -3,17 +3,13 @@ using DefaultNamespace.MathLib;
 
 namespace Manager.Scripts;
 
-public class Runner
+public partial class Runner // Ideen und bisschen ausgebaute Teile der Klasse in RunnerDeprecated
 {
     public string Name { get; set; }
     public int Money { get; set; }
 
     private double _fitness;
     private double _fatigue;
-    //private double _fatigueResistance;
-    //private double _hillSkill;
-    //private double _injuryProbability;
-    //private double _fatigueInterest = 1.05;
 
 
     public Runner(string name)
@@ -42,16 +38,13 @@ public class Runner
     private double GetFinishTime(Segment segment)
     {
         return segment.Distance *
-               GetAveragePace(); // * GetRoutePenaltyFactor(run) * GetRunnerReadienessFactor();
+               GetAveragePace();
     }
 
     private double CalculateFatigue(Segment segment)
     {
         double addedFatigue = segment.CalculateDifficultyScore();
-                              /* + _fatigue * _fatigueInterest
-                                + run.DistanceDifficultyScore
-                                + run.HillDifficultyScore / 10 / _hillSkill; */
-        return _fatigue + addedFatigue; // * _fatigueResistance;
+        return _fatigue + addedFatigue;
     }
 
     public void UpdateRunnerStats(Segment segment)
@@ -65,31 +58,4 @@ public class Runner
         return GetFinishTime(segment);
     }
 
-    /*
-     private double GetDistancePenaltyFactor(Run run)
-    {
-        return Math.Pow(1.1 - _fatigueResistance / 10, run.DistanceDifficultyScore);
-    }
-    */
-
-    /*
-     private double GetElevationPenaltyFactor(Run run)
-    {
-        return Math.Pow(1.1 - _hillSkill / 10, run.HillDifficultyScore / 100);
-    }
-    */
-
-    /*
-    private double GetRoutePenaltyFactor(Run run)
-    {
-        return GetDistancePenaltyFactor(run) * GetElevationPenaltyFactor(run);
-    }
-    */
-
-    /*
-    private double GetRunnerReadienessFactor()
-    {
-        return 1 / _fatigue;
-    }
-    */
 }
---
+++ src/Scripts/RunnerDeprecated.cs
@@ -0,0 +1,56 @@
+﻿namespace Manager.Scripts;
+
+public partial class Runner
+{
+    
+    //private double _fatigueResistance;
+    //private double _hillSkill;
+    //private double _injuryProbability;
+    //private double _fatigueInterest = 1.05;
+    
+    
+    /*
+     private double GetDistancePenaltyFactor(Run run)
+    {
+        return Math.Pow(1.1 - _fatigueResistance / 10, run.DistanceDifficultyScore);
+    }
+    */
+
+    /*
+     private double GetElevationPenaltyFactor(Run run)
+    {
+        return Math.Pow(1.1 - _hillSkill / 10, run.HillDifficultyScore / 100);
+    }
+    */
+
+    /*
+    private double GetRoutePenaltyFactor(Run run)
+    {
+        return GetDistancePenaltyFactor(run) * GetElevationPenaltyFactor(run);
+    }
+    */
+
+    /*
+    private double GetRunnerReadienessFactor()
+    {
+        return 1 / _fatigue;
+    }
+    */
+    
+    /*
+    private double GetFinishTime(Segment segment)
+    {
+        return segment.Distance *
+               GetAveragePace(); // * GetRoutePenaltyFactor(run) * GetRunnerReadienessFactor();
+    }
+
+    private double CalculateFatigue(Segment segment)
+    {
+        double addedFatigue = segment.CalculateDifficultyScore();
+         + _fatigue * _fatigueInterest
+          + run.DistanceDifficultyScore
+          + run.HillDifficultyScore / 10 / _hillSkill; 
+        return _fatigue + addedFatigue * _fatigueResistance;
+    }
+    */
+}
