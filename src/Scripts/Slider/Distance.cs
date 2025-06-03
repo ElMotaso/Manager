b8234a9f583fb1d274230623614bc65c81fb0bf8
Thomas Moissl <thomas.m.moissl@gmail.com>
2025-06-03 16:07:40 +0200

    bugfix slider does not work

--- src/Scripts/Slider/Distance.cs
+++ src/Scripts/Slider/Distance.cs
@@ -1,16 +1,20 @@
 using Godot;
 using System;
+using Manager.Scripts.Runs;
 
-   public partial class Distance : Node
-   {
-       [Export] private NodePath HSliderPath;
-       [Export] private NodePath TextEditPath;
+public partial class Distance : Node
+{
+    [Export] private NodePath _hSliderPath;
+    [Export] private NodePath _textEditPath;
       
-       public void _on_h_slider_drag_ended()
-       {
-           // Access the nodes via the exported paths
-           var hSlider = GetNode<HSlider>(HSliderPath);
-           var textEdit = GetNode<TextEdit>(TextEditPath);
-           textEdit.Text = hSlider.Value.ToString();
-       }
-   }
+    private void _on_h_slider_drag_ended(bool valueChanged)
+    {
+        if (valueChanged)
+        {
+            Console.WriteLine("jo wir sind da");
+            var hSlider = GetNode<HSlider>(_hSliderPath);
+            var textEdit = GetNode<TextEdit>(_textEditPath);
+            textEdit.Text = hSlider.Value.ToString();
+        }
+    }
+}
